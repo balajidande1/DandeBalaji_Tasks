ede42c795a5c0288ee9069e0ba72a728b4704a0f
Balaji Dande <balajidande7@gmail.com>
2024-07-29 20:15:12 +0530

    Task1 has been completed

---
+++ Task1.sql
@@ -0,0 +1,13 @@
+create database tasks;
+use tasks;
+
+create table students(
+s_id int primary key,
+s_name varchar(50),
+s_age tinyint
+);
+
+insert into students (s_id, s_name, s_age) values (1, 'Ram', 21);
+insert into students (s_id, s_name, s_age) values (2, 'Sharma', 21);
+
+select * from students;
